df9e0da3cedec45ab525fc76d8806bfa35a785e9
ayman aomari <aymanaomaripro@gmail.com>
2026-06-05 18:01:29 +0100

    greeting the customer

--- main.c
+++ main.c
@@ -3,9 +3,28 @@
 #include <string.h>
 
 
+
+void greetCustomer(const char* name) {
+    printf("Hello, Customer!\n");
+    if (name == NULL) {
+        fprintf(stderr, "Name cannot be NULL\n");
+        return;
+    }
+    printf("Welcome, %s!\n", name);
+}
+
+
 int main(){
-    printf("Hello, World!\n");
-    printf("This is a simple code for branch A");
-    print("Error");
+    
+    printf("Please enter your name: ");
+    char *customerName = malloc(100 * sizeof(char));
+    if (customerName == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
+    }
+    scanf("%s", customerName);
+    greetCustomer(customerName);
+
+    free(customerName);
     return 0;
 }
